b2b81e0980d233b3310eb0469bd1d0ba593f9052
Chance Tyler <chancebestqa@gmail.com>
2018-09-24 14:53:38 -0600

    created 2 additional tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -15,6 +15,29 @@ describe('Calculator', () => {
     assert.equal(result, 2)
   })
 
+  it('add two positive numbers together', () => {
+    // Arrange
+    const x = 2
+    const y = 4
+
+    // Act
+    const result = Calculator.add(y, x)
+
+    // Assert
+    assert.equal(result, 6)
+  })
+
+  it('add two positive numbers together', () => {
+    // Arrange
+    const x = (1 + 4)
+    const y = (5 + 1)
+
+    // Act
+    const result = Calculator.add(x, y)
+
+    // Assert
+    assert.equal(result, 11)
+  })
 
   // Testing for over exposure from the src/index.js file.
   // The js file doens't export the add function by default, and we must make it export in the export default
